71b040a16f84ceeb18e2eff8a1a173465c4e1429
davodhiambo <jesseomolo@yahoo.com>
2025-01-30 15:59:42 +0300

    feat: add RegistrationHandler.

--- handlers/register.go
+++ handlers/register.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func RegistrationHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/register.html")
+	if err != nil {
+		http.Error(w, "Unable to parse registration template", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, r); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
