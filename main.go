6ea25b4013b8a10f53f6bf251e26b1ce28779a04
davodhiambo <jesseomolo@yahoo.com>
2025-01-30 16:24:28 +0300

    feat: call handlers in main.go.

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"forum/handlers"
+)
+
+func main() {
+	// Candle hundler functions
+	http.HandleFunc("/static/", handlers.StaticHandler)
+	// http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/success", handlers.SuccessHandler)
+	http.HandleFunc("/error", handlers.ErrorHandler)
+	http.HandleFunc("/register", handlers.RegistrationHandler)
+
+	// Start the server, handle emerging errors
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("Failed to start server: ", err)
+		return
+	}
+
+	log.Println("Server started on port 8080")
 }
