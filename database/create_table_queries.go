a08c06eb4f71675985a7002f0e8cf71e9582b5a2
oduortoni <oduortoni@gmail.com>
2025-01-31 12:08:06 +0300

    feat: prevent table from being rectreated if it already exists
    add an IF NOT EXISTS to every query that initializes a table
    this is to prevent the table from being recreated eachtime we run the program

--- database/create_table_queries.go
+++ database/create_table_queries.go
@@ -1,7 +1,7 @@
 package database
 
 const (
-	USERS_TABLE_CREATE = `CREATE TABLE users (
+	USERS_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY,
 		username STRING UNIQUE NOT NULL,
 		email STRING UNIQUE NOT NULL,
@@ -11,26 +11,26 @@ const (
 		session_id STRING
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);`
-	USERS_TABLE_INDEX_username = `CREATE INDEX idx_users_username ON users (username);`
-	USERS_TABLE_INDEX_email = `CREATE INDEX idx_users_email ON users (email);`
-	USERS_TABLE_INDEX_session_id = `CREATE INDEX idx_users_session_id ON users (session_id);`
+	USERS_TABLE_INDEX_username = `CREATE INDEX IF NOT EXISTS idx_users_username ON users (username);`
+	USERS_TABLE_INDEX_email = `CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);`
+	USERS_TABLE_INDEX_session_id = `CREATE INDEX IF NOT EXISTS idx_users_session_id ON users (session_id);`
 	
-	SESSION_TABLE_CREATE = `CREATE TABLE sessions (
+	SESSION_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS sessions (
 		session_id STRING PRIMARY KEY,
 		expiry TIMESTAMP NOT NULL,
 		user_id INTEGER NOT NULL,
 		FOREIGN KEY (user_id) REFERENCES users(id)
 	);`
-	SESSION_TABLE_INDEX_user_id = `CREATE INDEX idx_sessions_user_id ON sessions (user_id);`
-	SESSION_TABLE_INDEX_expiry = `CREATE INDEX idx_sessions_expiry ON sessions (expiry);`
+	SESSION_TABLE_INDEX_user_id = `CREATE INDEX IF NOT EXISTS idx_sessions_user_id ON sessions (user_id);`
+	SESSION_TABLE_INDEX_expiry = `CREATE INDEX IF NOT EXISTS idx_sessions_expiry ON sessions (expiry);`
 	
-	CATEGORIES_TABLE_CREATE = `CREATE TABLE categories (
+	CATEGORIES_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS categories (
 		id INTEGER PRIMARY KEY,
 		name STRING UNIQUE NOT NULL
 	);`
-	CATEGORIES_TABLE_INDEX_name = `CREATE INDEX idx_categories_name ON categories (name);`
+	CATEGORIES_TABLE_INDEX_name = `CREATE INDEX IF NOT EXISTS idx_categories_name ON categories (name);`
 	
-	POSTS_TABLE_CREATE = `CREATE TABLE posts (
+	POSTS_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS posts (
 		id INTEGER PRIMARY KEY,
 		title STRING NOT NULL,
 		content STRING NOT NULL,
@@ -38,19 +38,19 @@ const (
 		user_id INTEGER NOT NULL,
 		FOREIGN KEY (user_id) REFERENCES users(id)
 	);`
-	POSTS_TABLE_INDEX_user_id = `CREATE INDEX idx_posts_user_id ON posts (user_id);`
+	POSTS_TABLE_INDEX_user_id = `CREATE INDEX IF NOT EXISTS idx_posts_user_id ON posts (user_id);`
 	
-	POST_CATEGORIES_TABLE_CREATE = `CREATE TABLE post_categories (
+	POST_CATEGORIES_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS post_categories (
 		post_id INTEGER NOT NULL,
 		category_id INTEGER NOT NULL,
 		PRIMARY KEY (post_id, category_id),
 		FOREIGN KEY (post_id) REFERENCES posts(id),
 		FOREIGN KEY (category_id) REFERENCES categories(id)
 	);`
-	POST_CATEGORIES_TABLE_INDEX_post_id = `CREATE INDEX idx_post_categories_post_id ON post_categories (post_id);`
-	POST_CATEGORIES_TABLE_INDEX_category_id = `CREATE INDEX idx_post_categories_category_id ON post_categories (category_id);`
+	POST_CATEGORIES_TABLE_INDEX_post_id = `CREATE INDEX IF NOT EXISTS idx_post_categories_post_id ON post_categories (post_id);`
+	POST_CATEGORIES_TABLE_INDEX_category_id = `CREATE INDEX IF NOT EXISTS idx_post_categories_category_id ON post_categories (category_id);`
 	
-	COMMENTS_TABLE_CREATE = `CREATE TABLE comments (
+	COMMENTS_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS comments (
 		id INTEGER PRIMARY KEY,
 		content STRING NOT NULL,
 		post_id INTEGER NOT NULL,
@@ -58,10 +58,10 @@ const (
 		FOREIGN KEY (post_id) REFERENCES posts(id),
 		FOREIGN KEY (user_id) REFERENCES users(id)
 	);`
-	COMMENTS_TABLE_INDEX_user_id = `CREATE INDEX idx_comments_user_id ON comments (user_id);`
-	COMMENTS_TABLE_INDEX_post_id = `CREATE INDEX idx_comments_post_id ON comments (post_id);`
+	COMMENTS_TABLE_INDEX_user_id = `CREATE INDEX IF NOT EXISTS idx_comments_user_id ON comments (user_id);`
+	COMMENTS_TABLE_INDEX_post_id = `CREATE INDEX IF NOT EXISTS idx_comments_post_id ON comments (post_id);`
 	
-	LIKES_TABLE_CREATE = `CREATE TABLE likes (
+	LIKES_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS likes (
 		id INTEGER PRIMARY KEY,
 		user_id INTEGER NOT NULL,
 		post_id INTEGER,
@@ -71,10 +71,10 @@ const (
 		FOREIGN KEY (comment_id) REFERENCES comments(id),
 		CHECK (post_id IS NOT NULL OR comment_id IS NOT NULL)
 	);`
-	LIKES_TABLE_INDEX_post_id = `CREATE INDEX idx_likes_post_id ON likes (post_id);`
-	LIKES_TABLE_INDEX_comment_id = `CREATE INDEX idx_likes_comment_id ON likes (comment_id);`
+	LIKES_TABLE_INDEX_post_id = `CREATE INDEX IF NOT EXISTS idx_likes_post_id ON likes (post_id);`
+	LIKES_TABLE_INDEX_comment_id = `CREATE INDEX IF NOT EXISTS idx_likes_comment_id ON likes (comment_id);`
 	
-	DISLIKES_TABLE_CREATE = `CREATE TABLE dislikes (
+	DISLIKES_TABLE_CREATE = `CREATE TABLE IF NOT EXISTS dislikes (
 		id INTEGER PRIMARY KEY,
 		user_id INTEGER NOT NULL,
 		post_id INTEGER,
@@ -84,6 +84,6 @@ const (
 		FOREIGN KEY (comment_id) REFERENCES comments(id),
 		CHECK (post_id IS NOT NULL OR comment_id IS NOT NULL)
 	);`
-	DISLIKES_TABLE_INDEX_post_id = `CREATE INDEX idx_dislikes_post_id ON dislikes (post_id);`
-	DISLIKES_TABLE_INDEX_comment_id = `CREATE INDEX idx_dislikes_comment_id ON dislikes (comment_id);`
+	DISLIKES_TABLE_INDEX_post_id = `CREATE INDEX IF NOT EXISTS idx_dislikes_post_id ON dislikes (post_id);`
+	DISLIKES_TABLE_INDEX_comment_id = `CREATE INDEX IF NOT EXISTS idx_dislikes_comment_id ON dislikes (comment_id);`
 )
