ba50e5c80a4efad0d57963be72406ebe0f686e65
oduortoni <oduortoni@gmail.com>
2025-02-01 21:35:56 +0300

    feat: extract user creation in tests to the MainTest function
    We can now test using a single user prepopulated in the database
    This user's id is needed to do everytinh that requires authentication

--- database/main_test.go
+++ database/main_test.go
@@ -18,13 +18,27 @@ func deleteTestDb() {
 	}
 }
 
-
+// a special function used to setup testing
 func TestMain(m *testing.M) {
 	// Setup before tests
 	fmt.Println("Global setup: init db handle")
+
+	// delete the database file used in tests if available
 	deleteTestDb()
+
+	// inititlize all tables
 	Init(dbFile)
+
+	// with db opened, ensure it will be closed once tests are done
 	defer db.Close()
+
+	// create a single user that will be used in tests
+	_, err = CreateUser("milton", "milton@mail.com", "mPass")
+	if err != nil {
+		error_s := fmt.Errorf("%w\n", err)
+		fmt.Println(error_s)
+		os.Exit(1)
+	}
 
 	// Run tests
 	code := m.Run()
