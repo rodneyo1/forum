1add011bc289f2ef35e9cfe3430b1ce5a057df76
davodhiambo <jesseomolo@yahoo.com>
2025-02-02 03:49:47 +0300

    refactor: adjust LoginHandler to handle invalid username-password combinations.

--- handlers/login.go
+++ handlers/login.go
@@ -1,49 +1,98 @@
 package handlers
 
 import (
-	"forum/models"
 	"html/template"
 	"log"
 	"net/http"
 	"strings"
+
+	"forum/database"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	var user models.User
+	// Construct absolute path to login.html
+	templatePath, err := GetTemplatePath("login.html")
+	if err != nil {
+		InternalServerErrorHandler(w)
+		log.Println("Could not find template file: ", err)
+		return
+	}
+
+	// When method is GET, render form
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles(templatePath)
+		if err != nil {
+			http.Error(w, "Failed to load Login template", http.StatusInternalServerError)
+			return
+		}
+
+		if err := tmpl.Execute(w, r); err != nil {
+			InternalServerErrorHandler(w)
+			return
+		}
+		return
+	}
 
 	// Restrict requests to "GET" or "POST"
-	if !(r.Method == "POST" || r.Method == "GET") {
+	if r.Method != "POST" {
 		BadRequestHandler(w)
 		log.Println("LoginHandler ERROR: Bad request")
 		return
 	}
 
-	// Render the login template with the request object as data
-	tmpl, err := template.ParseFiles("web/templates/login.html")
+	// Parse form data from the request
+	err = r.ParseForm()
 	if err != nil {
-		http.Error(w, "Failed to load Login template", http.StatusInternalServerError)
+		BadRequestHandler(w)
+		log.Println("Invalid form submission", http.StatusBadRequest)
 		return
 	}
 
-	// Populate user credentials
-	// Determine whether input is a valid email
-	if IsValidEmail(r.FormValue("email_username")) {
-		user.Email = r.FormValue("email_username")
-	} else {
-		user.Username = r.FormValue("username")
+	// Extract form data
+	emailUsername := r.FormValue("email_username")
+	password := r.FormValue("password")
+
+	// Validate credentials
+	if !database.VerifyUser(emailUsername, password) {
+		// Define template path and error message
+		alert := map[string]string{"Error Message": "Invalid password"}
+		tmplPath, err := GetTemplatePath("login.html")
+		if err != nil {
+			InternalServerErrorHandler(w)
+			log.Println("Could not find template file", err)
+			return
+		}
+
+		// Render login page
+		tmpl, err := template.ParseFiles(tmplPath)
+		if err != nil {
+			InternalServerErrorHandler(w)
+			log.Printf("Could not parse template: %v", err)
+			return
+		}
+
+		// Execute login page
+		err = tmpl.Execute(w, alert)
+		if err != nil {
+			InternalServerErrorHandler(w)
+			log.Printf("Could not execute template %v", err)
+			return
+		}
 	}
 
-	user.Password = r.FormValue("password") // Populate password field
-	log.Println("Username: ", user.Username)
-	log.Println("Email: ", user.Email)
-	log.Println("Password: ", user.Password)
-
-	if err := tmpl.Execute(w, r); err != nil {
-		InternalServerErrorHandler(w)
-		return
-	}
+	// Redirect user to home page
+	http.Redirect(w, r, "/success", http.StatusFound)
 }
 
 func IsValidEmail(email string) bool {
 	return strings.Contains(email, "@") && strings.HasSuffix(email, ".com")
+}
+
+func SuccessHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		BadRequestHandler(w)
+		return
+	}
+
+	http.Error(w, "Loged in1!", http.StatusInternalServerError)
 }
