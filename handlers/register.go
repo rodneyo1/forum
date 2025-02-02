dff2292888db79a2888a954c28eb4c1fa103f715
anne-okingo <annekadiso@gmail.com>
2025-02-02 12:40:38 +0300

    feat := check if method is allowed

--- handlers/register.go
+++ handlers/register.go
@@ -6,6 +6,12 @@ import (
 )
 
 func RegistrationHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the method is allowed (e.g., POST)
+	if !MethodCheck(w, r, http.MethodPost) {
+		// If the method is not allowed, stop further processing
+		return
+	}
+
 	tmpl, err := template.ParseFiles("web/templates/register.html")
 	if err != nil {
 		http.Error(w, "Unable to parse registration template", http.StatusInternalServerError)
