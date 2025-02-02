500a23c73683cbba06a56f2d4eaa6f731d62fa4c
anne-okingo <annekadiso@gmail.com>
2025-02-02 18:02:06 +0300

    feat: get form values from registerHandler

--- handlers/register.go
+++ handlers/register.go
@@ -3,15 +3,31 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"forum/utils"
 )
 
 func RegistrationHandler(w http.ResponseWriter, r *http.Request) {
 	// Check if the method is allowed (e.g., POST)
-	if !MethodCheck(w, r, http.MethodPost) {
-		// If the method is not allowed, stop further processing
+	if MethodCheck(w, r, http.MethodGet) {
+		// Render the registration form
+		tmpl, err := template.ParseFiles("web/templates/register.html")
+		if err != nil {
+			http.Error(w, "Unable to load registration page", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
 		return
 	}
 
+	if MethodCheck(w, r, http.MethodPost) {
+		// Parse form values and validate user input
+		user, err := utils.PaerseUserForm(r)
+		if err != nil {
+			http.Error(w, err.Issue, err.Code)
+			return
+		}
+		utils.Passwordhash(user)
+	}
 	tmpl, err := template.ParseFiles("web/templates/register.html")
 	if err != nil {
 		http.Error(w, "Unable to parse registration template", http.StatusInternalServerError)
