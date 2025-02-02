f3956a44f9d68231354572dd352717542a66a408
anne-okingo <annekadiso@gmail.com>
2025-02-02 12:39:52 +0300

    feat : check if method is allowed

--- handlers/login.go
+++ handlers/login.go
@@ -6,6 +6,11 @@ import (
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the method is allowed (e.g., POST)
+	if !MethodCheck(w, r, http.MethodPost) {
+		// If the method is not allowed, stop further processing
+		return
+	}
 	tmpl, err := template.ParseFiles("web/templates/login.html")
 	if err != nil {
 		http.Error(w, "Failed to load Login template", http.StatusInternalServerError)
