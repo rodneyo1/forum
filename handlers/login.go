9541c130dae2b0469a694a1f252f2c4db55536de
davodhiambo <jesseomolo@yahoo.com>
2025-02-02 05:02:24 +0300

    chore: simplify code by reusing code.

--- handlers/login.go
+++ handlers/login.go
@@ -17,14 +17,15 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Render login.html
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		http.Error(w, "Failed to load Login template", http.StatusInternalServerError)
+		return
+	}
+
 	// When method is GET, render form
 	if r.Method == "GET" {
-		tmpl, err := template.ParseFiles(templatePath)
-		if err != nil {
-			http.Error(w, "Failed to load Login template", http.StatusInternalServerError)
-			return
-		}
-
 		if err := tmpl.Execute(w, r); err != nil {
 			InternalServerErrorHandler(w)
 			return
@@ -53,7 +54,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Validate credentials
 	if !database.VerifyUser(emailUsername, password) {
-		ParseAlertMessage(w, templatePath, "Invalid Username or Password") // Parse error message
+		ParseAlertMessage(w, tmpl, templatePath, "Invalid Username or Password") // Parse error message
 		return
 	}
 
@@ -70,20 +71,12 @@ func SuccessHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Loged in1!", http.StatusInternalServerError)
 }
 
-func ParseAlertMessage(w http.ResponseWriter, tmplPath, message string) {
+func ParseAlertMessage(w http.ResponseWriter, tmpl *template.Template, tmplPath, message string) {
 	// Define template path and error message
 	alert := map[string]string{"ErrorMessage": message}
 
-	// Render page
-	tmpl, err := template.ParseFiles(tmplPath)
-	if err != nil {
-		InternalServerErrorHandler(w)
-		log.Printf("Could not parse template: %v", err)
-		return
-	}
-
 	// Execute page
-	err = tmpl.Execute(w, alert)
+	err := tmpl.Execute(w, alert)
 	if err != nil {
 		InternalServerErrorHandler(w)
 		log.Printf("Could not execute template %v", err)
