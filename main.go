ad8dcb33f8076421e4e67726485a0c0181406d84
rodnochieng <rodneyotieno27@gmail.com>
2025-02-04 16:28:51 +0300

    create user profile

--- main.go
+++ main.go
@@ -38,8 +38,11 @@ func main() {
 	http.HandleFunc("/forgot-password", handlers.ForgotPasswordHandler)
 	http.HandleFunc("/register", handlers.RegistrationHandler)
 	http.Handle("/posts/create", middleware.AuthMiddleware(http.HandlerFunc(postHandlers.PostCreate)))
-	// http.Handle("/posts/", middleware.AuthMiddleware(http.HandlerFunc(postHandlers.DisplaySinglePost)))
-	http.HandleFunc("/posts/display", postHandlers.PostDisplay)
+	http.Handle("/posts/", middleware.AuthMiddleware(http.HandlerFunc(postHandlers.DisplaySinglePost)))
+	http.HandleFunc(" /posts/display", postHandlers.PostDisplay)
+	// User Profile routes
+	http.HandleFunc("GET /profile", handlers.ViewUserProfile)
+	// http.HandleFunc("GET /user/update", middleware.AuthMiddleware(http.HandlerFunc(handlers.UpdateUserProfile))) // Protected
 
 	// Inform user initialization of server
 	log.Println("Server started on port 8080")
