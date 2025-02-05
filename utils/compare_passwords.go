9e4da7f87ae2a264e21c1d080c175b32de68c940
davodhiambo <jesseomolo@yahoo.com>
2025-02-05 15:11:33 +0300

    fix: update MatchPasswords to return both boolean and error values.

--- utils/compare_passwords.go
+++ utils/compare_passwords.go
@@ -3,7 +3,14 @@ package utils
 import "golang.org/x/crypto/bcrypt"
 
 // Uses bcrypt to compare hashed and non-hashed passwords
-func MatchPasswords(hashedPassword, inputPassword string) bool {
+func MatchPasswords(hashedPassword, inputPassword string) (bool, error) {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inputPassword))
-	return err == nil
+	if err != nil {
+		// Confirm if error is due to mismatching passwords
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
