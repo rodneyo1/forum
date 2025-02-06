878195295224551f599ebb412b1ca4dae0d1221a
anoduor <oduortoni@gmail.com>
2025-02-06 17:04:37 +0300

    fix: likes and dislkes
    add the number of likes for the post
    add the functionality for liking and disliking a comment
    fix the logic such that a user can either like or dislike a post or a comment once

--- database/get_all_posts.go
+++ database/get_all_posts.go
@@ -4,9 +4,19 @@ import (
 	"forum/models"
 )
 
-// fetches all posts from the database with the creator's names
+// fetches all posts from the database with the creator's names and the number of likes and dislikes
 func GetAllPosts() ([]models.PostWithUsername, error) {
-	query := `SELECT p.uuid, p.title, p.content, p.media, p.created_at, u.username FROM posts p JOIN users u ON u.id = p.user_id`
+	// SQL query to fetch posts along with like and dislike counts
+	query := `
+		SELECT p.uuid, p.title, p.content, p.media, p.created_at, u.username,
+			COALESCE(COUNT(l.id), 0) AS likes_count, 
+			COALESCE(COUNT(d.id), 0) AS dislikes_count
+		FROM posts p
+		JOIN users u ON u.id = p.user_id
+		LEFT JOIN likes l ON l.post_id = p.uuid
+		LEFT JOIN dislikes d ON d.post_id = p.uuid
+		GROUP BY p.uuid, u.username
+	`
 	rows, err := db.Query(query)
 	if err != nil {
 		return nil, err
@@ -16,7 +26,7 @@ func GetAllPosts() ([]models.PostWithUsername, error) {
 	var posts []models.PostWithUsername
 	for rows.Next() {
 		var post models.PostWithUsername
-		err := rows.Scan(&post.UUID, &post.Title, &post.Content, &post.Media, &post.CreatedAt, &post.Creator)
+		err := rows.Scan(&post.UUID, &post.Title, &post.Content, &post.Media, &post.CreatedAt, &post.Creator, &post.LikesCount, &post.DislikesCount)
 		if err != nil {
 			return nil, err
 		}
