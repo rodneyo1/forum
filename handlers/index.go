6bae57393912094264ba7490a5ec13bcd1a29911
rodnochieng <rodneyotieno27@gmail.com>
2025-02-06 16:12:17 +0300

    add category data to homepage

--- handlers/index.go
+++ handlers/index.go
@@ -15,14 +15,20 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to load posts", http.StatusInternalServerError)
 		return
 	}
-
+	categories, err := database.FetchCategories()
+	if err != nil {
+		http.Error(w, "Failed to fetch categories", http.StatusInternalServerError)
+		return
+	}
 	// Load the HTML template
 	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
 	// Execute the template with the posts data
 	data := struct {
 		Posts []models.PostWithUsername
+		Categories []models.Category
 	}{
 		Posts: posts,
+		Categories: categories,
 	}
 	tmpl.Execute(w, data)
 }
