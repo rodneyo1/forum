d9776f0265726c39009da516c430f4df28843dd2
davodhiambo <jesseomolo@yahoo.com>
2025-02-11 10:15:08 +0300

    merge: reconcile conflicts.

--- handlers/users/profile.go
+++ handlers/users/profile.go
@@ -19,6 +19,7 @@ func ViewUserProfile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	userData, err := database.GetUserbySessionID(session.SessionID)
+	log.Printf("%q", userData)
 	// fmt.Printf("UserData retrieved: %+v\n", userData)  // Add debug logging
 	if err != nil {
 		log.Printf("Error getting user: %v\n", err) // Add error logging
