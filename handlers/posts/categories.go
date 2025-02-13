008aaa1b101c5d5b7a52c4135cd7dc433a8c2de5
rodnochieng <rodneyotieno27@gmail.com>
2025-02-13 13:09:40 +0300

    improve error handling

--- handlers/posts/categories.go
+++ handlers/posts/categories.go
@@ -76,15 +76,17 @@ func SingeCategoryPosts(w http.ResponseWriter, r *http.Request) {
 	categoryID := pathParts[2]
 	ID, err := strconv.Atoi(categoryID)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		//log.Println(err)
+		//w.WriteHeader(http.StatusInternalServerError)
+		errors.BadRequestHandler(w)
+		//http.Redirect(w,r,"/", http.StatusSeeOther)
 		return
 	}
 	// Fetch posts from the database
 	posts, err := database.FetchCategoryPostsWithID(ID)
 	if err != nil {
-		log.Println("Category doesn't exist")
-		errors.InternalServerErrorHandler(w)
+		log.Println(err)
+		errors.NotFoundHandler(w)
 		return
 	}
 	// Load the HTML template
@@ -96,7 +98,7 @@ func SingeCategoryPosts(w http.ResponseWriter, r *http.Request) {
 	}
 	// Execute the template with the posts
 	data := struct {
-		Posts    []models.Post
+		Posts    []models.PostWithCategories
 		IsLogged bool
 		ProfPic  string
 	}{
