2b891b6b39832840b481326169e925788e155e0d
davodhiambo <jesseomolo@yahoo.com>
2025-02-16 03:47:48 +0300

    feat: add MethodNotAllowedHandler.

--- handlers/errors/bad_request.go
+++ handlers/errors/bad_request.go
@@ -9,29 +9,28 @@ import (
 )
 
 // Serves Bad Request error page
-func MethodNotAllowedHandler(w http.ResponseWriter) {
+func BadRequestHandler(w http.ResponseWriter) {
 	// Construct absolute path to error.html
 	tmplPath, err := utils.GetTemplatePath("error.html")
 	if err != nil {
-		http.Error(w, "error page unavailable", http.StatusNotFound)
-		log.Printf("TEMPLATE AVAILABILITY ERROR: %v", err)
+		log.Printf("Could not find template file: %v", err)
 		return
 	}
 
 	tmpl, err := template.ParseFiles(tmplPath)
 	if err != nil {
-		log.Println("TEMPLATE PARSING ERROR:", err)
+		log.Println("Template parsing failed:", err)
 		http.Error(w, "Could not load template, error page unavailable", http.StatusInternalServerError)
 		return
 	}
 
 	// Set relevant headers
-	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.WriteHeader(http.StatusBadRequest)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	// Set parameters of error
 	hitch.Code = http.StatusBadRequest
-	hitch.Issue = "Method Not Allowed!"
+	hitch.Issue = "Bad Request!"
 
 	// Execute bad request template, handle emerging errors
 	err = tmpl.Execute(w, hitch)
